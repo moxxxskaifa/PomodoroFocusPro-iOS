3ec82950446fe314a2eefbbf886cbc6d7707f1c5
moxxxskaifa <moxxxskaifa@gmail.com>
2026-06-10 22:32:56 +0800

    fix: iPad iOS26 crash - use AppDelegate, disable multiple scenes

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,18 +1,36 @@
 import SwiftUI
 import ComposeApp
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .ignoresSafeArea(.all)
         }
     }
 }
 
 struct ContentView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        let controller = MainViewControllerKt.MainViewController()
+        controller.view.backgroundColor = UIColor(red: 0.06, green: 0.06, blue: 0.14, alpha: 1.0)
+        return controller
     }
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
